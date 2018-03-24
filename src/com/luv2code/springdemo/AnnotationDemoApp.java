ac7a1172819ddc6a2b11653d8c1e5035497853e2
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-03-25 00:55:23 +0100

    retriving bean from config file = (context) = (container)

---
+++ src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -0,0 +1,21 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+
+    public static void main(String[] args) {
+
+//        Read the spring config file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+//        Retrieve the bean from Spring container
+        Coach coach = context.getBean("thatSillyCoach", Coach.class);
+
+//        Call methods on the bean
+        System.out.println(coach.getDailyWorkout());
+
+//        Close the context
+        context.close();
+    }
+}
