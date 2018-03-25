dbbd9ed83b07015ef13feefbc61fdf4d57104fcd
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-03-25 03:12:01 +0200

    Scope - Prototype (different objects references)

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,6 +1,7 @@
 package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 /**
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
  * thatSillyCoach - bean id
  */
 @Component
+@Scope("prototype")
 public class TennisCoach implements Coach {
 
     @Autowired
